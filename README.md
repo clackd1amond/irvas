a4a672e718a78dd4993348e61910d1024cdba8b2
Eugene <clackd1amond@users.noreply.github.com>
2021-04-07 10:45:31 +0300

    modals

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# irvas" 
+# irvas
+
+- Modals
---
+++ src/js/modules/modals.js
@@ -0,0 +1,59 @@
+const modals = () => {
+	function bindModal(triggerSelector, modalSelector, closeSelector) {
+		const trigger = document.querySelectorAll(triggerSelector),
+			modal = document.querySelector(modalSelector),
+			close = document.querySelector(closeSelector);
+
+		trigger.forEach((item) => {
+			item.addEventListener('click', (e) => {
+				if (e.target) {
+					e.preventDefault();
+				}
+
+				openModal(modal);
+			});
+		});
+
+		close.addEventListener('click', () => closeModal(modal));
+
+		modal.addEventListener('click', (e) => {
+			if (e.target === modal) {
+				closeModal(modal);
+			}
+		});
+
+		document.addEventListener('keydown', (e) => {
+			if (
+				e.code === 'Escape' &&
+				document.body.classList.contains('modal-open')
+			) {
+				closeModal(modal);
+			}
+		});
+	}
+
+	function openModal(modal) {
+		modal.style.display = 'block';
+		document.body.classList.add('modal-open');
+	}
+	function closeModal(modal) {
+		modal.style.display = 'none';
+		document.body.classList.remove('modal-open');
+	}
+	function showModalByTime(selector, time) {
+		setTimeout(() => {
+			openModal(document.querySelector(selector));
+		}, time);
+	}
+
+	bindModal(
+		'.popup_engineer_btn',
+		'.popup_engineer',
+		'.popup_engineer .popup_close'
+	);
+
+	bindModal('.phone_link', '.popup', '.popup .popup_close');
+	showModalByTime('.popup', 3000);
+};
+
+export default modals;
